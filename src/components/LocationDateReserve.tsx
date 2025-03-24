0c183d8fb7be7c525a9b60661c7ebce9bd8ee465
Pareena Chirattikun <pareenachirattikun@gmail.com>
2025-03-24 23:44:26 +0700

    Update LocationDateReserve.tsx

--- src/components/LocationDateReserve.tsx
+++ src/components/LocationDateReserve.tsx
@@ -1,59 +1,38 @@
 "use client"
-import LocationDateReserve from "@/components/LocationDateReserve";
-import { useSearchParams } from "next/navigation";
-import { useState } from "react";
-import dayjs, { Dayjs } from "dayjs";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "@/redux/store";
-import { ReservationItem } from "../../../interfaces";
-import { addReservation } from "@/redux/features/cartSlice";
+import { useState } from "react"
+import { DatePicker } from "@mui/x-date-pickers"
+import { LocalizationProvider } from "@mui/x-date-pickers"
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs"
+import { Select, MenuItem } from "@mui/material"
+import { Dayjs } from "dayjs"
 
-export default function Reservation () {
+export default function LocationDateReserve({onDateChange, onLocationChange}
+    :{onDateChange:Function, onLocationChange:Function} ) {
 
-    const urlParams = useSearchParams()
-    const cid = urlParams.get('id')
-    const model = urlParams.get('model')
-
-    const dispatch = useDispatch<AppDispatch>()
-
-    const makeReservation = () => {
-        if(cid && model && pickupDate && returnDate) {
-            const item:ReservationItem = {
-                carId: cid,
-                carModel: model,
-                numOfDays: returnDate.diff(pickupDate, 'day'),
-                pickupDate: dayjs(pickupDate).format("YYYY/MM/DD"),
-                pickupLocation: pickupLocation,
-                returnDate: dayjs(returnDate).format("YYYY/MM/DD"),
-                returnLocation: returnLocation
-            }
-            dispatch(addReservation(item))
-        }
-    }
-
-    const [pickupDate, setPickupDate] = useState<Dayjs|null>(null)
-    const [pickupLocation, setPickupLocation] = useState<string>("BKK")
-    const [returnDate, setReturnDate] = useState<Dayjs|null>(null)
-    const [returnLocation, setReturnLocation] = useState<string>("BKK")
+    const [reserveDate, setReserveDate] = useState<Dayjs|null>(null)
+    const [location, setLocation] = useState('BKK')
 
     return (
-        <main className="w-[100%] flex flex-col items-center space-y-4">
-            <div className="text-xl font-medium">Make a reservation</div>
-            
+        <div className="bg-slate-100 rounded-lg space-x-5 space-y-2
+        w-fit px-10 py-5 flex flex-row justify-center">
 
-            <div className="w-fit space-y-2">
-                <div className="text-md text-left text-gray-600 ">Select Date and Restaurant</div>
-                <LocationDateReserve onDateChange={(value:Dayjs)=>{setPickupDate(value)}}
-                onLocationChange={(value:string)=>{setPickupLocation(value)}}/>
-       
-            </div>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker className="bg-white"
+                value={reserveDate} 
+                onChange={(value)=>{setReserveDate(value); onDateChange(value)}}
+                />
+            </LocalizationProvider>
 
-            <button className="block rounded-md bg-red-800 hover:bg-red-600 px-10 py-2
-            text-white shadow-small" onClick={makeReservation}>
-                Reserve this Restaurant
-            </button> 
-            
-        </main>
-
-    );
+            <Select variant="standard" 
+            name="location" id="location" value={location}
+            onChange={(e)=>{setLocation(e.target.value); onLocationChange(e.target.value);}}
+            className="h-[2em] w-[200px]">
+                <MenuItem value="BKK">R1</MenuItem>
+                <MenuItem value="CNX">R2</MenuItem>
+                <MenuItem value="HKT">R3</MenuItem>
+                <MenuItem value="CNX">R3</MenuItem>
+                <MenuItem value="HKT">R4</MenuItem>
+            </Select>
+        </div>
+    )
 }
